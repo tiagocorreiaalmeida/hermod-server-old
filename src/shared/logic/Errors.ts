aa7ecf5f191284f932ff1bd7d5d880e982769c16
brvno <bhenrique.contact@gmail.com>
2020-11-01 16:32:32 -0300

    feat: add base Error template functions

---
+++ src/shared/logic/Errors.ts
@@ -0,0 +1,3 @@
+export const missingParamError = (paramName: string): string => `Missing param: ${paramName}`;
+
+export const invalidParamError = (paramName: string): string => `Invalid param: ${paramName}`;
